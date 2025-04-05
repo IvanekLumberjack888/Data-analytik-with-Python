7e9f221463e48450240e466d54cb99a7c1de2af3
Ivanek Lumberjack <ivousd@gmail.com>
2025-04-05 15:52:05 +0200

    Update text_analyzer.py
    okkaz s popisem

--- Projekty/text_analyzer/text_analyzer.py
+++ Projekty/text_analyzer/text_analyzer.py
@@ -1,2 +1,2 @@
 ## Text analyzer
-<https://github.com/IvanekLumberjack888/Data-analytik-with-Python/tree/text_analyzer>
+[Odkaz na větev text_analyzer](https://github.com/IvanekLumberjack888/Data-analytik-with-Python/tree/text_analyzer)
