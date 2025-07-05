80c00589822b4b097731b58f61caf291bec600eb
Ivanek Lumberjack <ivousd@gmail.com>
2025-07-05 11:54:22 +0200

    Add files via upload

---
+++ STUDIUM👨‍🏫/modul_2/lekce_06/5. 7. 25 - 1.py
@@ -0,0 +1,22 @@
+seznam = [1, 2, 3]
+iterator = iter(seznam)
+print(next(iterator))  # 1
+print(next(iterator))  # 2
+
+seznam_tymu = ["Tým A", "Tým B", "Tým C"]
+polozka = ""
+for i, polozka in enumerate(seznam_tymu, start=0):
+    print(f"{i}: {polozka}")
+
+# jablka a hrušky
+ovoce = {"ovoce": ["jablko", "hruška", "banán"], "zelenina": ["mrkev", "brambora", "cibule"]}
+for i, (polozka) in enumerate(ovoce["ovoce"], start=1):
+    print(f"{i}: {polozka}")
+
+else:
+    # Přidání dalších položek do seznamu ovoce
+    ovoce["ovoce"].extend(["kiwi", "pomeranč", "broskev"])
+    
+    for i, polozka in enumerate(ovoce["ovoce"], start=1):
+        print(f"{i}: {polozka}")
+# Výstup: 0: jablko, 1: hruška, 2: banán
